111a0bb553674296413ec9bc7d5c7304a11cdbe8
Maxime-LERAS <maxime.leras@etu.unice.fr>
2019-12-23 15:28:01 +0100

    Scenario 2 with deadlocks

--- main.py
+++ main.py
@@ -198,31 +198,31 @@ def getFieldFromPosAndDirection(direction, p):
 def acquireLockFromFieldNumber(x):
     if x == 1:
         one.acquire()
-        print("one is locked")
+        #print("one is locked")
     elif x == 2:
         two.acquire()
-        print("two is locked")
+        #print("two is locked")
     elif x == 3:
         three.acquire()
-        print("three is locked")
+        #print("three is locked")
     elif x == 4:
         four.acquire()
-        print("four is locked")
+        #print("four is locked")
 
 
 def releaseLockFromFieldNumber(x):
     if x == 1:
         one.release()
-        print("one is unlocked")
+        #print("one is unlocked")
     elif x == 2:
         two.release()
-        print("two is unlocked")
+        #print("two is unlocked")
     elif x == 3:
         three.release()
-        print("three is unlocked")
+        #print("three is unlocked")
     elif x == 4:
         four.release()
-        print("four is unlocked")
+        #print("four is unlocked")
 
 
 def getListFromFieldNumber(x):
@@ -242,184 +242,73 @@ def subAlgorithm2(number):
 
     # # Sort people by the distance as the crow flies from the exit
     while len(people1) != 0 or len(people2) != 0 or len(people3) != 0 or len(people4) != 0:
+        print(str(number) +"->" + str(people1) + " " + str(people2) + " " + str(people3) + " " + str(people4))
         if number == 1:
             if len(people1) != 0:
                 one.acquire()
-                print("chuis le 1")
+                #print("one is locked")
                 for p in people1:
                     if isOnExit(p[0], p[1]):  # in front of the exit
                         people1.remove(p)
+                        #print(str(number) +"->" + str(people1) + " " + str(people2) + " " + str(people3) + " " + str(people4))
                     if canGoHereField(p[0] - 1, p[1] - 1, 1):  # can do a diagonal movement
+                        #print(str(p) + "moving to [" + str(p[0]-1) + "," + str(p[1] - 1) + "]")
                         p[0], p[1] = p[0] - 1, p[1] - 1
                     elif canGoHereField(p[0] - 1, p[1], 1):  # can do an horizontal movement
+                        #print(str(p) + "moving to [" + str(p[0]-1) + "," + str(p[1]) + "]")
                         p[0], p[1] = p[0] - 1, p[1]
                     elif canGoHereField(p[0], p[1] - 1, 1):  # can do a vertical movement
+                        #print(str(p) + "moving to [" + str(p[0]) + "," + str(p[1] - 1) + "]")
                         p[0], p[1] = p[0], p[1] - 1
                 one.release()
-                print("1 est unlocké")
+                #print("one is unlocked")
         else:
             if len(getListFromFieldNumber(number)) != 0:
                 acquireLockFromFieldNumber(number)
-                print("chuis le " + str(number))
                 for p in getListFromFieldNumber(number):
                     diagonalField = getFieldFromPosAndDirection("diagonal", p)
                     horizontalField = getFieldFromPosAndDirection("horizontal", p)
                     verticalField = getFieldFromPosAndDirection("vertical", p)
                     if canGoHereField(p[0] - 1, p[1] - 1, diagonalField):  # can do a diagonal movement
-                        if diagonalField == getListFromFieldNumber(number):
+                        if diagonalField == number:
+                            #print(str(p) + "moving to [" + str(p[0]-1) + "," + str(p[1] - 1) + "]")
                             p[0], p[1] = p[0] - 1, p[1] - 1
                         else:  # p is changing field
                             acquireLockFromFieldNumber(diagonalField)
-                            if canGoHereField(p[0] - 1, p[1] - 1,
-                                              diagonalField):  # rechecking in case of something changed
+                            if canGoHereField(p[0] - 1, p[1] - 1,diagonalField):  # rechecking in case of something changed
+                                #print(str(p) + "moving to [" + str(p[0]-1) + "," + str(p[1] - 1) + "] new field " + str(diagonalField))
                                 getListFromFieldNumber(number).remove(p)  # removing the person from the field
                                 getListFromFieldNumber(diagonalField).append(
                                     [p[0] - 1, p[1] - 1])  # adding the person to the new field
                             releaseLockFromFieldNumber(diagonalField)
 
                     elif canGoHereField(p[0] - 1, p[1], horizontalField):  # can do an horizontal movement
-                        if horizontalField == getListFromFieldNumber(number):
+                        if horizontalField == number:
+                            #print(str(p) + "moving to [" + str(p[0]-1) + "," + str(p[1]) + "]")
                             p[0], p[1] = p[0] - 1, p[1]
                         else:
                             acquireLockFromFieldNumber(horizontalField)
                             if canGoHereField(p[0] - 1, p[1],
                                               horizontalField):  # rechecking in case of something changed
+                                #print(str(p) + "moving to [" + str(p[0]-1) + "," + str(p[1]) + "] new field " + str(horizontalField))
                                 getListFromFieldNumber(number).remove(p)  # removing the person from the field
                                 getListFromFieldNumber(horizontalField).append(
                                     [p[0] - 1, p[1]])  # adding the person to the new field
                             releaseLockFromFieldNumber(horizontalField)
 
                     elif canGoHereField(p[0], p[1] - 1, verticalField):  # can do a vertical movement
-                        if verticalField == getListFromFieldNumber(number):
+                        if verticalField == number:
+                            #print(str(p) + "moving to [" + str(p[0]) + "," + str(p[1] - 1) + "]")
                             p[0], p[1] = p[0], p[1] - 1
                         else:
                             acquireLockFromFieldNumber(verticalField)
                             if canGoHereField(p[0], p[1] - 1, verticalField):  # rechecking in case of something changed
+                                #print(str(p) + "moving to [" + str(p[0]) + "," + str(p[1] - 1) + "] new field " + str(verticalField))
                                 getListFromFieldNumber(number).remove(p)  # removing the person from the field
                                 getListFromFieldNumber(verticalField).append(
                                     [p[0], p[1] - 1])  # adding the person to the new field
                             releaseLockFromFieldNumber(verticalField)
                 releaseLockFromFieldNumber(number)
-        # elif number == 2:
-        #     two.acquire()
-        #     print("chuis le 2")
-        #     for p in people2:
-        #         diagonalField = getFieldFromPosAndDirection("diagonal", p)
-        #         horizontalField = getFieldFromPosAndDirection("horizontal", p)
-        #         verticalField = getFieldFromPosAndDirection("vertical", p)
-        #         if canGoHereField(p[0] - 1, p[1] - 1, diagonalField):  # can do a diagonal movement
-        #             if diagonalField == 2:
-        #                 p[0], p[1] = p[0] - 1, p[1] - 1
-        #             else:  # p is changing field
-        #                 acquireLockFromFieldNumber(diagonalField)
-        #                 if canGoHereField(p[0] - 1, p[1] - 1, diagonalField):  # rechecking in case of something changed
-        #                     people2.remove(p)  # removing the person from the field
-        #                     getListFromFieldNumber(diagonalField).append(
-        #                         [p[0] - 1, p[1] - 1])  # adding the person to the new field
-        #                 releaseLockFromFieldNumber(diagonalField)
-        #
-        #         elif canGoHereField(p[0] - 1, p[1], horizontalField):  # can do an horizontal movement
-        #             if horizontalField == 2:
-        #                 p[0], p[1] = p[0] - 1, p[1]
-        #             else:
-        #                 acquireLockFromFieldNumber(horizontalField)
-        #                 if canGoHereField(p[0] - 1, p[1], horizontalField):  # rechecking in case of something changed
-        #                     people2.remove(p)  # removing the person from the field
-        #                     getListFromFieldNumber(horizontalField).append(
-        #                         [p[0] - 1, p[1]])  # adding the person to the new field
-        #                 releaseLockFromFieldNumber(horizontalField)
-        #
-        #         elif canGoHereField(p[0], p[1] - 1, verticalField):  # can do a vertical movement
-        #             if verticalField == 2:
-        #                 p[0], p[1] = p[0], p[1] - 1
-        #             else:
-        #                 acquireLockFromFieldNumber(verticalField)
-        #                 if canGoHereField(p[0], p[1] - 1 , verticalField):  # rechecking in case of something changed
-        #                     people2.remove(p)  # removing the person from the field
-        #                     getListFromFieldNumber(verticalField).append(
-        #                         [p[0], p[1] - 1])  # adding the person to the new field
-        #                 releaseLockFromFieldNumber(verticalField)
-        #     two.release()
-        # elif number == 3:
-        #     three.acquire()
-        #     print("chuis le 3")
-        #     for p in people3:
-        #         diagonalField = getFieldFromPosAndDirection("diagonal", p)
-        #         horizontalField = getFieldFromPosAndDirection("horizontal", p)
-        #         verticalField = getFieldFromPosAndDirection("vertical", p)
-        #         if canGoHereField(p[0] - 1, p[1] - 1, diagonalField):  # can do a diagonal movement
-        #             if diagonalField == 3:
-        #                 p[0], p[1] = p[0] - 1, p[1] - 1
-        #             else:  # p is changing field
-        #                 acquireLockFromFieldNumber(diagonalField)
-        #                 if canGoHereField(p[0] - 1, p[1] - 1, diagonalField):  # rechecking in case of something changed
-        #                     people3.remove(p)  # removing the person from the field
-        #                     getListFromFieldNumber(diagonalField).append(
-        #                         [p[0] - 1, p[1] - 1])  # adding the person to the new field
-        #                 releaseLockFromFieldNumber(diagonalField)
-        #
-        #         elif canGoHereField(p[0] - 1, p[1], horizontalField):  # can do an horizontal movement
-        #             if horizontalField == 3:
-        #                 p[0], p[1] = p[0] - 1, p[1]
-        #             else:
-        #                 acquireLockFromFieldNumber(horizontalField)
-        #                 if canGoHereField(p[0] - 1, p[1], horizontalField):  # rechecking in case of something changed
-        #                     people3.remove(p)  # removing the person from the field
-        #                     getListFromFieldNumber(horizontalField).append(
-        #                         [p[0] - 1, p[1]])  # adding the person to the new field
-        #                 releaseLockFromFieldNumber(horizontalField)
-        #
-        #         elif canGoHereField(p[0], p[1] - 1, verticalField):  # can do a vertical movement
-        #             if verticalField == 3:
-        #                 p[0], p[1] = p[0], p[1] - 1
-        #             else:
-        #                 acquireLockFromFieldNumber(verticalField)
-        #                 if canGoHereField(p[0], p[1] - 1, verticalField):  # rechecking in case of something changed
-        #                     people3.remove(p)  # removing the person from the field
-        #                     getListFromFieldNumber(verticalField).append(
-        #                         [p[0], p[1] - 1])  # adding the person to the new field
-        #                 releaseLockFromFieldNumber(verticalField)
-        #     three.release()
-        # elif number == 4:
-        #     four.acquire()
-        #     print("chuis le 4")
-        #     for p in people4:
-        #         diagonalField = getFieldFromPosAndDirection("diagonal", p)
-        #         horizontalField = getFieldFromPosAndDirection("horizontal", p)
-        #         verticalField = getFieldFromPosAndDirection("vertical", p)
-        #         if canGoHereField(p[0] - 1, p[1] - 1, diagonalField):  # can do a diagonal movement
-        #             if diagonalField == 4:
-        #                 p[0], p[1] = p[0] - 1, p[1] - 1
-        #             else:  # p is changing field
-        #                 acquireLockFromFieldNumber(diagonalField)
-        #                 if canGoHereField(p[0] - 1, p[1] - 1, diagonalField):  # rechecking in case of something changed
-        #                     people4.remove(p)  # removing the person from the field
-        #                     getListFromFieldNumber(diagonalField).append(
-        #                         [p[0] - 1, p[1] - 1])  # adding the person to the new field
-        #                 releaseLockFromFieldNumber(diagonalField)
-        #
-        #         elif canGoHereField(p[0] - 1, p[1], horizontalField):  # can do an horizontal movement
-        #             if horizontalField == 4:
-        #                 p[0], p[1] = p[0] - 1, p[1]
-        #             else:
-        #                 acquireLockFromFieldNumber(horizontalField)
-        #                 if canGoHereField(p[0] - 1, p[1], horizontalField):  # rechecking in case of something changed
-        #                     people4.remove(p)  # removing the person from the field
-        #                     getListFromFieldNumber(horizontalField).append(
-        #                         [p[0] - 1, p[1]])  # adding the person to the new field
-        #                 releaseLockFromFieldNumber(horizontalField)
-        #
-        #         elif canGoHereField(p[0], p[1] - 1, verticalField):  # can do a vertical movement
-        #             if verticalField == 4:
-        #                 p[0], p[1] = p[0], p[1] - 1
-        #             else:
-        #                 acquireLockFromFieldNumber(verticalField)
-        #                 if canGoHereField(p[0], p[1] - 1, verticalField):  # rechecking in case of something changed
-        #                     people4.remove(p)  # removing the person from the field
-        #                     getListFromFieldNumber(verticalField).append(
-        #                         [p[0], p[1] - 1])  # adding the person to the new field
-        #                 releaseLockFromFieldNumber(verticalField)
-        #     four.release()
 
 
 def algorithm2():
@@ -502,7 +391,17 @@ def main():
         timeCPUval.pop()
         print("Execution time: %ss" % (mean(timeval)))
         print("CPU time: %ss" % (mean(timeCPUval)))
-
+    else:
+        if (scenario != 2):
+            initialization()
+        else:
+            init4Fields()
+        if scenario == 0:
+            algorithm0()
+        if scenario == 1:
+            algorithm1()
+        if scenario == 2:
+            algorithm2()
     exit(0)
 
 
